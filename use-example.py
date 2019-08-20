6082ff01d0d91a9fc0e5e5fbb370a962d4b741e2
Tom A.V <tomiav97@gmail.com>
2019-08-20 16:14:36 +0200

    custom input and config dialog example

--- use-example.py
+++ use-example.py
@@ -1,11 +1,20 @@
-from customQWidgets import cQWidgetTableGB, cQCustomInput, cQWidgetTable
+from customQWidgets import cQWidgetTableGB, cQCustomInput, cQWidgetTable, cQConfigDialog
 from PyQt5.QtWidgets import QTableWidget, QApplication, QMainWindow, QLabel, QPushButton
 import sys
 
-def connect():
-    print("dummy")
+def opendialog():
+  options=["beef", "tuna", "bacon"]
+  comFormWidgets=[["Dropdown", cQCustomInput(arg_optionList=options,  arg_defaultValue=2, parent=None)],
+                  ["Number",   cQCustomInput(arg_numberRange=[0,100], arg_defaultValue=50,parent=None)],
+                  ["Text",     cQCustomInput(arg_defaultValue="default text", parent=None)]]
+  
+  info, ok=cQConfigDialog.promptDialog(arg_settings=comFormWidgets)
+  if(ok):
+    print("Dropdown: %s" % options[info[0]])
+    print("Number: %i" % info[1])
+    print("Text: %s" % info[2])
 def start():
-    print("dummy")
+  print("dummy")
 def stop():
     print("dummy")
 def cancel():
@@ -16,10 +25,10 @@ app = QApplication(sys.argv)
 window = QMainWindow()
 window.setWindowTitle('Button box')
 
-buttonTable=[[          QPushButton("Connect")            ],
+buttonTable=[[          QPushButton("Open dialog")        ],
              [QPushButton("Start"),  QPushButton("Stop")  ],
              [       "span"      , QPushButton("Cancel") ]]
-buttonTable[0][0].clicked.connect(connect)
+buttonTable[0][0].clicked.connect(opendialog)
 buttonTable[0][0].setMaximumSize(1000,1000)
 buttonTable[1][0].clicked.connect(start)
 buttonTable[1][0].setMaximumSize(1000,1000)
